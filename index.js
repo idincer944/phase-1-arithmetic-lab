eab1f4948f5654e1e10ce14d5f22b58489c4e584
İsmail <idincer944@gmail.com>
2023-03-15 18:57:15 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+// Multiply
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+multiply;
+
+// Random
+let random = Math.floor(Math.random() * 10) + 1;
+random;
+
+// Mod
+const num3 = 14;
+const num4 = 10;
+let mod = num3 % num4;
+mod;
+
+// Max
+let max = Math.max(5, 8, 9, 15, 20);
+max; 
